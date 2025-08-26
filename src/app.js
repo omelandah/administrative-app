5065746bfe0e5413aad86d7ad7aa9829117dd25e
anhnt276 <ntanh0393@gmail.com>
2025-08-26 18:23:34 +0700

    connect to mysql db server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 const routes = require('./routes');
 
@@ -10,6 +11,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', routes);
+
+async function main() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
+  const [rows] = await connection.execute('SELECT NOW() AS now');
+  console.log('⏰ Time:', rows[0].now);
+
+  await connection.end();
+}
+
+main();
 
 app.listen(PORT, () => {
   console.log('Connected to port ', PORT);
