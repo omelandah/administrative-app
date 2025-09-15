4247feb0a85c7f4350213ddd237abae4339d3293
anhnt276 <ntanh0393@gmail.com>
2025-09-16 06:19:19 +0700

    update sequelize config

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require('fs');
 
 module.exports = {
   development: {
@@ -8,5 +9,18 @@ module.exports = {
     host: process.env.DB_HOST || 'db',
     port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(process.env.DB_SSL_CA),
+      },
+    },
   },
 };
